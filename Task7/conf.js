a325378902f527a7664bd748ed7c3dab5a4269f6
Ani Sargsyan <Ani_Sargsyan@epam.com>
2019-03-22 17:02:16 +0400

    add 'se-node-chrome-3.4.0' and 'se-node-firefox-3.4.0'

--- Task7/conf.js
+++ Task7/conf.js
@@ -3,7 +3,7 @@ const GLOBAL_TIMEOUT = 40e3;
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['./src/spec/spec.js'],
+  specs: ['./src/spec/*.js'],
   capabilities: {
     browserName: 'chrome',
   },
@@ -11,9 +11,6 @@ exports.config = {
     global.GLOBAL_TIMEOUT = GLOBAL_TIMEOUT;
     global.ec = protractor.ExpectedConditions;
 
-    const chai = require('chai');
-    chai.use(require('chai-as-promised'));
-    global.expect = chai.expect;
 
     browser.manage().window().maximize();
     browser.waitForAngularEnabled(false);
